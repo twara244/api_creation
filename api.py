5749f8bb2dd1c13d8d43bc652ddab1dacf4376f5
twara244 <140725784+twara244@users.noreply.github.com>
2025-01-13 14:23:25 +0530

    Update api.py
    added api key

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
+
+API_KEY = os.environ.get("API_KEY")
 
 @app.route("/")
 def hello_world():
@@ -8,6 +11,11 @@ def hello_world():
 
 @app.route('/check',methods=['GET'])
 def check_query():
+      
+    user_api_key = request.headers.get('X-API-Key')  # Check the API key from the request header
+    if user_api_key is None or user_api_key != API_KEY:
+        return jsonify({"error": "Unauthorized"}), 401
+
     n= request.args.get('n',type=int)
     word=request.args.get('word',type=str)
 
